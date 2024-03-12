623ffae5c2bad9bf6f1a7b42971640ddba902ed2
theGamingKitten <56452901+theGamingKitten@users.noreply.github.com>
2024-03-13 00:54:55 +0100

    switch prices annually <-> monthly

---
+++ script.js
@@ -0,0 +1,12 @@
+const priceMonthlySpans = document.querySelectorAll('.price-monthly');
+const priceAnnuallySpans = document.querySelectorAll('.price-annually');
+const switchPrices = document.getElementsByTagName('input')[0];
+
+switchPrices.addEventListener("click", () => {
+    priceMonthlySpans.forEach(span => {
+        span.classList.toggle("hidden");
+    });
+    priceAnnuallySpans.forEach(span => {
+        span.classList.toggle("hidden");
+    })
+});
